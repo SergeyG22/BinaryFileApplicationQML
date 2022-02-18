5489d3afd962e551bc59f9e08d099237d10c42e2
SergeyGavrilov <lastleafautumn@yandex.ru>
2022-02-18 20:58:07 +0300

    catalog update

--- src/Backend/backend.cpp
+++ src/Backend/backend.cpp
@@ -16,11 +16,13 @@ QVariantMap BackendOperations::binaryFindOperation(QString pathA, QString pathB)
         if (boost::filesystem::exists(boost::filesystem::path(folderA)) &&
             boost::filesystem::exists(boost::filesystem::path(folderB))) {
 
-            for (auto& firstIt : boost::filesystem::directory_iterator(folderA)) {
-                if (!boost::filesystem::is_directory(firstIt.path())) {
-                    if (boost::filesystem::file_size(firstIt.path())) {
+            for(auto firstIt = boost::filesystem::recursive_directory_iterator(folderA);
+                firstIt != boost::filesystem::recursive_directory_iterator(); ++firstIt) {
+
+                if (!boost::filesystem::is_directory(firstIt->path())) {
+                    if (boost::filesystem::file_size(firstIt->path())) {
                         fileA.close();
-                        fileA.open(firstIt.path());
+                        fileA.open(firstIt->path());
                     }
                     else {
                         fileA.close();
@@ -31,12 +33,13 @@ QVariantMap BackendOperations::binaryFindOperation(QString pathA, QString pathB)
                     fileA.close();
                 }
 
+                for(auto secondIt = boost::filesystem::recursive_directory_iterator(folderB);
+                    secondIt != boost::filesystem::recursive_directory_iterator(); ++secondIt) {
 
-                for (auto& secondIt : boost::filesystem::directory_iterator(folderB)) {
-                    if (!boost::filesystem::is_directory(secondIt.path())) {
-                        if (boost::filesystem::file_size(secondIt.path())) {
+                    if (!boost::filesystem::is_directory(secondIt->path())) {
+                        if (boost::filesystem::file_size(secondIt->path())) {
                             fileB.close();
-                            fileB.open(secondIt.path());
+                            fileB.open(secondIt->path());
                         }
                         else {
                             fileB.close();
@@ -52,9 +55,9 @@ QVariantMap BackendOperations::binaryFindOperation(QString pathA, QString pathB)
 
                             if (fileA.size() == fileB.size() && boost::algorithm::equal(fileA.data(), fileA.data() + fileA.size(), fileB.data(),
                                 fileB.data() + fileB.size())) {
-                                  QString item = QString::fromUtf8(firstIt.path().filename().string().c_str())
+                                  QString item = QString::fromUtf8(firstIt->path().filename().string().c_str())
                                                  + " [EQUIALENT] " +
-                                                 QString::fromUtf8(secondIt.path().filename().string().c_str());
+                                                 QString::fromUtf8(secondIt->path().filename().string().c_str());
                                   lines.insert(QString::number(position),item);
                                   position++;
                             }
